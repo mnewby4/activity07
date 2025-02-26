d79e26d848d8fd51ffc709b8bcc514a5933c7a89
mylanewby <167246119+mylanewby@users.noreply.github.com>
2025-02-26 14:18:44 -0500

    Animation changes depending on the page

--- lib/main.dart
+++ lib/main.dart
@@ -25,32 +25,34 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
       _isVisible = !_isVisible;
     });
   }
-  
-
-
 
   Widget createItem(String text, Color color) {
     return Scaffold(
       backgroundColor: color,
-      body: Column(
-          children: <Widget>[
-            AnimatedOpacity(
-              opacity: _isVisible ? 1.0 : 0.0,
-              duration: Duration(seconds: 1),
-              child: const Text(
-                'Hello, Flutter!',
-                style: TextStyle(fontSize: 24),
-              ),
-            ),
-            AnimatedCrossFade(
-              duration: const Duration(seconds: 3),
-              firstChild: const FlutterLogo(style: FlutterLogoStyle.horizontal, size: 100.0),
-              secondChild: const FlutterLogo(style: FlutterLogoStyle.stacked, size: 100.0),
-              crossFadeState: _isVisible ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-            )],
+      body: Container(child:
+          Builder(
+            builder: (context) {
+              if (text == "First Page") 
+                return AnimatedOpacity(
+                  opacity: _isVisible ? 1.0 : 0.0,
+                  duration: Duration(seconds: 1),
+                  child: const Text(
+                    'Hello, Flutter!',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                );
+
+              return AnimatedCrossFade(
+                duration: const Duration(seconds: 3),
+                firstChild: const FlutterLogo(style: FlutterLogoStyle.horizontal, size: 100.0),
+                secondChild: const FlutterLogo(style: FlutterLogoStyle.stacked, size: 100.0),
+                crossFadeState: _isVisible ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+              );
+            },
           ),
-        );
-      }
+        ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,16 +61,16 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
         title: Text('Fading Text Animation'),
       ),
       body: 
-      PageView(
-        children: <Widget>[
-          createItem('First Page', Colors.red),
-          createItem('Second Page', Colors.blue),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: toggleVisibility,
-        child: Icon(Icons.play_arrow),
-      ),      
+        PageView(
+          children: <Widget>[
+            createItem('First Page', Colors.red),
+            createItem('Second Page', Colors.blue),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: toggleVisibility,
+          child: Icon(Icons.play_arrow),
+        ),      
     );
   }
 }
