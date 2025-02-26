40fa0f41dc7ea90be7db418256e8d501ac93f65d
mylanewby <167246119+mylanewby@users.noreply.github.com>
2025-02-26 14:07:43 -0500

    Added another fading animation and ability to swipe screens

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,37 @@ class FadingTextAnimation extends StatefulWidget {
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
-
   void toggleVisibility() {
     setState(() {
       _isVisible = !_isVisible;
     });
   }
+  
+
+
+
+  Widget createItem(String text, Color color) {
+    return Scaffold(
+      backgroundColor: color,
+      body: Column(
+          children: <Widget>[
+            AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              duration: Duration(seconds: 1),
+              child: const Text(
+                'Hello, Flutter!',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            AnimatedCrossFade(
+              duration: const Duration(seconds: 3),
+              firstChild: const FlutterLogo(style: FlutterLogoStyle.horizontal, size: 100.0),
+              secondChild: const FlutterLogo(style: FlutterLogoStyle.stacked, size: 100.0),
+              crossFadeState: _isVisible ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+            )],
+          ),
+        );
+      }
 
   @override
   Widget build(BuildContext context) {
@@ -33,20 +58,17 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
       appBar: AppBar(
         title: Text('Fading Text Animation'),
       ),
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
+      body: 
+      PageView(
+        children: <Widget>[
+          createItem('First Page', Colors.red),
+          createItem('Second Page', Colors.blue),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: toggleVisibility,
         child: Icon(Icons.play_arrow),
-      ),
+      ),      
     );
   }
 }
